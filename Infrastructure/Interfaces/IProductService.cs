e11eb52a0b08feeb49b07fc2c2eb021cc12595c0
NicolasSossou <sossounicolas846@gmail.com>
2025-09-18 10:05:50 +0200

    IProductService and ProductResult Model Created

---
+++ Infrastructure/Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces;
+
+public interface IProductService
+{
+
+    ProductResult AddProductToList(Product product);
+   ProductResult<IEnumerable<Product>> GetAllProducts(); // Så att den returnerar en lista av produkter se till att vänja dig med detta//
+    
+}
