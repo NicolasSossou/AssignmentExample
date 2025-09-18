fd5fe3e924c1dc01d6cdc7260deb91e702c89ea9
NicolasSossou <sossounicolas846@gmail.com>
2025-09-18 10:14:31 +0200

    Added FileResult

---
+++ Infrastructure/Interfaces/IFileService.cs
@@ -0,0 +1,12 @@
+﻿
+using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces;
+
+public interface IFileService
+{
+    FileResult SaveContentToFile(string path, string content);
+    FileResult GetContentFromFile(string path);
+
+}
+// Sökväg til filen: Infrastructure/Interfaces/IFileService.cs
