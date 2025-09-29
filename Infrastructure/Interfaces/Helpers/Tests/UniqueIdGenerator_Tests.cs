84e2f8b9cbe529f5f5e19394bcce7a8d6bbb7e52
NicolasSossou <sossounicolas846@gmail.com>
2025-09-29 12:34:57 +0200

    New Class Product

---
+++ Infrastructure/Interfaces/Helpers/Tests/UniqueIdGenerator_Tests.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Interfaces.Helpers;
+namespace Infrastructure.Interfaces.Helpers.Tests;
+
+public class UniqueIdGenerator_Tests
+{
+    [Fact]
+
+   
+    public void Generate_ShouldReturnGuidtoString_WhenSuccessful()
+    { 
+        // Act
+        string result = UniqueIdGenerator.Generate();
+        // Assert
+        Assert.NotEmpty(result);
+        Assert.True(Guid.TryParse(result, out _));
+    }
+}
